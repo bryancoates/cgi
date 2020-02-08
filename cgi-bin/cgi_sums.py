28ed57a8b8857b1dd5121178a2be8af0c262502a
Bryan Coates <bcoates@halowerks.com>
2020-02-08 11:25:12 -0800

    Added CGI activity

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,21 @@ import cgitb
 
 cgitb.enable()
 
+# Get data from query string
+form = cgi.FieldStorage()
+values = form.getlist('value')
+
+# Add the values together
+try:
+    total = 0
+    for value in values:
+        total += int(value)
+
+    body = f"Total: {total}"
+except (ValueError, TypeError):
+    body = "No data, or bad type.  Please provide integer values in query string."
+
+# Display the content/body
 print("Content-type: text/plain")
-print()
-print("Your job is to make this work")
+print("")
+print(body)
